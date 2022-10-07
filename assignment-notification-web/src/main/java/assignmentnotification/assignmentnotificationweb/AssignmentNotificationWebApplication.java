a45c705672a3f5719208900cd04a87c01d5bb7e3
dongsseop2 <76763417+dongseoplee@users.noreply.github.com>
2022-10-07 15:38:20 +0900

    start spring boot

---
+++ assignment-notification-web/src/main/java/assignmentnotification/assignmentnotificationweb/AssignmentNotificationWebApplication.java
@@ -0,0 +1,13 @@
+package assignmentnotification.assignmentnotificationweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssignmentNotificationWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AssignmentNotificationWebApplication.class, args);
+	}
+
+}
---
+++ assignment-notification-web/src/test/java/assignmentnotification/assignmentnotificationweb/AssignmentNotificationWebApplicationTests.java
@@ -0,0 +1,13 @@
+package assignmentnotification.assignmentnotificationweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssignmentNotificationWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
